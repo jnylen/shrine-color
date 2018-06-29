50b330000981b90dcdbfc33692c84f00df30a716
Guillaume Loubier <gloubier@o2web.ca>
2018-06-29 11:57:19 -0400

    Update specs

--- spec/shrine/color_spec.rb
+++ spec/shrine/color_spec.rb
@@ -2,7 +2,10 @@ require 'spec_helper'
 
 describe Shrine::Plugins::Color do
   before do
-    @uploader = uploader { plugin :color }
+    @uploader = uploader {
+      plugin :determine_mime_type
+      plugin :color
+    }
   end
 
   describe ":dominant_color" do
@@ -17,13 +20,25 @@ describe Shrine::Plugins::Color do
     it "extracts palette_color from files with default hexes via add_metadata" do
       @uploader.class.add_metadata(:palette_color) { |io, context| palette_color(io.path) }
       uploaded_file = @uploader.upload(File.open("spec/fixtures/bora.jpg"))
-      expect(uploaded_file.metadata.fetch("palette_color")).to eq("#333399")
+      expect(uploaded_file.metadata.fetch("palette_color")).to eq(["#333399"])
     end
 
     it "extracts palette_color from files with custom hexes via add_metadata" do
-      @uploader.class.add_metadata(:palette_color) { |io, context| palette_color(io.path, ['ff0000', '00ff00', '0000ff']) }
+      @uploader.class.add_metadata(:palette_color) { |io, context| palette_color(io.path, 1, ['ff0000', '00ff00', '0000ff']) }
       uploaded_file = @uploader.upload(File.open("spec/fixtures/bora.jpg"))
-      expect(uploaded_file.metadata.fetch("palette_color")).to eq("#0000ff")
+      expect(uploaded_file.metadata.fetch("palette_color")).to eq(["#0000ff"])
+    end
+
+    it "extracts 3 palette_colors from files with default hexes via add_metadata" do
+      @uploader.class.add_metadata(:palette_color) { |io, context| palette_color(io.path, 3) }
+      uploaded_file = @uploader.upload(File.open("spec/fixtures/bora.jpg"))
+      expect(uploaded_file.metadata.fetch("palette_color")).to eq(["#333399", "#0066cc", "#663399"])
+    end
+
+    it "extracts 3 palette_colors from files with custom hexes via add_metadata" do
+      @uploader.class.add_metadata(:palette_color) { |io, context| palette_color(io.path, 3, ['ff0000', '00ff00', '0000ff']) }
+      uploaded_file = @uploader.upload(File.open("spec/fixtures/bora.jpg"))
+      expect(uploaded_file.metadata.fetch("palette_color")).to eq(["#0000ff", "#00ff00"])
     end
   end
 
